136089b6d1c749d125dc33276122d7fd2c7146d5
Natali <nattalli.gf@gmail.com>
2023-10-28 12:08:50 +0300

    create: project, install dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client/react';
+import { BrowserRouter} from 'react-router-dom';
+import { store } from './store/store';
+import client from "./apollo/client";
+import App from './app/App';
 import './index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<ApolloProvider client={client}>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
